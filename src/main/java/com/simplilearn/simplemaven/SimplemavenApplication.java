dc21825f95774898460d89972db0f13a59f50845
mallela009 <mallelaarjuna009@gmail.com>
2022-11-30 15:22:45 +0530

    first commit

---
+++ src/main/java/com/simplilearn/simplemaven/SimplemavenApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.simplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemavenApplication.class, args);
+	}
+
+}
